964310e9cacddc4dd9134468b527e1539bcc6f67
A KRISHNA SRIKAR <71891009+AKRISHNASRIKAR@users.noreply.github.com>
2023-11-28 02:08:13 +0530

    Update score(pre).c

--- score(pre).c
+++ score(pre).c
@@ -1,42 +1,42 @@
 #include <stdio.h>
+#include <conio.h> // This header file includes getch and clrscr functions
 
-// Function to display the ball-wise scores in a table
-void displayScoreTable(int ballWiseScores[], int overs)
+#define MAX_OVERS 50
+#define MAX_BATSMEN 11
 
- {
-    printf("\n Ball-wise Score Table: \n");
+void displayScoreTable(int overWiseScores[MAX_OVERS], int overs)
+{
+    printf("\n Over-wise Score Table: \n");
     printf("-------------------------------\n");
-    printf("||  Ball  ||  Score  ||  Total  ||\n");
+    printf("||  Over  ||  Score  ||  Total  ||\n");
     printf("-------------------------------\n");
 
     int totalScore = 0;
 
-    for (int i = 0; i < overs * 6; ++i)
-     {
-        totalScore += ballWiseScores[i];
-        printf("|   %2d   |   %2d    |   %3d   |\n", i + 1, ballWiseScores[i], totalScore);
+    for (int i = 0; i < overs; ++i)
+    {
+        totalScore += overWiseScores[i];
+        printf("|   %d   |   %3d    |   %3d   |\n", i + 1, overWiseScores[i], totalScore);
     }
 
     printf("-------------------------------\n");
 }
 
 int main()
-
- {
+{
     int team_A_Score = 0;
     int wickets = 0;
     int overs = 0;
     int choice;
     int runs;
 
-    // Maximum number of balls in an innings (assuming 6 balls per over)
-    int maxBalls = 50 * 6;
+    int overWiseScores[MAX_OVERS] = {0};
+    int batsmenScores[MAX_BATSMEN] = {0};
 
-    // Array to store ball-wise scores
-    int ballWiseScores[maxBalls];
-   
-    while (overs < 50 && wickets < 10)
-     {
+    while (overs < MAX_OVERS && wickets < 10)
+    {
+        clrscr(); // Clear the screen
+
         printf("\nCricket Score Display\n");
         printf("1. Run Scored\n");
         printf("2. Wicket Fell\n");
@@ -47,42 +47,40 @@ int main()
         scanf("%d", &choice);
 
         switch (choice)
-         {
-            case 1:
-                printf("Enter runs scored in this ball: ");
-                scanf("%d", &runs);
+        {
+        case 1:
+            printf("Enter runs scored in this over: ");
+            scanf("%d", &runs);
 
-                // Store the run in the array
-                ballWiseScores[overs * 6 + (overs % 6)] = runs;
+            overWiseScores[overs] = runs;
+            team_A_Score += runs;
+            overs += 1;
+            break;
 
-                team_A_Score += runs;
-                overs += 1; // error but still have to go ahead with this
-            
-                break;
+        case 2:
+            wickets += 1;
+            overs += 1;
+            break;
 
-            case 2:
-                wickets += 1;
-                overs += 1;
-                break;
+        case 3:
+            printf("Team A Score: %d/%d in %d overs\n", team_A_Score, wickets, overs);
+            displayScoreTable(overWiseScores, overs);
+            break;
 
-            case 3:
-                printf("Team A Score: %d/%d in %d overs\n", team_A_Score, wickets, overs);
-                displayScoreTable(ballWiseScores, overs);
-                break;
+        case 4:
+            printf("Innings over. Team A scored %d for %d wickets in %d overs.\n", team_A_Score, wickets, overs);
+            displayScoreTable(overWiseScores, overs);
+            getch(); // Wait for a key press before exiting
+            return 0;
 
-            case 4:
-                printf("Innings over. Team A scored %d for %d wickets in %d overs.\n", team_A_Score, wickets, overs);
-                displayScoreTable(ballWiseScores, overs);
-                return 0;
-
-            default:
-                printf("Invalid choice. Please enter a valid option.\n");
+        default:
+            printf("Invalid choice. Please enter a valid option.\n");
         }
     }
 
     printf("Innings over. Team A scored %d for %d wickets in %d overs.\n", team_A_Score, wickets, overs);
-    displayScoreTable(ballWiseScores, overs);
-    
-    
+    displayScoreTable(overWiseScores, overs);
+    getch(); // Wait for a key press before exiting
+
     return 0;
 }
